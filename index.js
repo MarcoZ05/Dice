95c0a45bf18d4c74ffd6cd658dacd624596ac429
Käkäsh <marco.zillgen23@gmail.com>
2022-02-19 18:12:09 +0100

    update

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
-let diceWay = ["UP","UP","UP","UP","UP","UP"]
-let diceNow = [4];
-let diceUp = [5];
-let diceRight = [1];
-let sumOfDiceWay = diceNow[0];
+let diceWay = []
+let diceNow = [];
+let diceUp = [];
+let diceRight = [];
+let sumOfDiceWay = 0;
 
 function simulateDiceWay(diceWayIndex) {
   if (diceWay[diceWayIndex] == "UP") {
     sumOfDiceWay += diceUp[diceWayIndex];
-    diceUp[diceWayIndex+1] = otherSideOfDice(diceNow[diceWayIndex]);
-    diceNow[diceWayIndex+1] = diceUp[diceWayIndex];
+    diceUp[diceWayIndex + 1] = otherSideOfDice(diceNow[diceWayIndex]);
+    diceNow[diceWayIndex + 1] = diceUp[diceWayIndex];
   } else if (diceWay[diceWayIndex] == "RIGHT") {
     sumOfDiceWay += diceRight[diceWayIndex];
-    diceRight[diceWayIndex+1] = otherSideOfDice(diceNow[diceWayIndex]);
-    diceNow[diceWayIndex+1] = diceRight[diceWayIndex];
+    diceRight[diceWayIndex + 1] = otherSideOfDice(diceNow[diceWayIndex]);
+    diceNow[diceWayIndex + 1] = diceRight[diceWayIndex];
   } else if (diceWay[diceWayIndex] == "DOWN") {
     sumOfDiceWay += otherSideOfDice(diceUp[diceWayIndex]);
-    diceUp[diceWayIndex+1] = diceNow[diceWayIndex];
-    diceNow[diceWayIndex+1] = otherSideOfDice(diceUp[diceWayIndex]);
+    diceUp[diceWayIndex + 1] = diceNow[diceWayIndex];
+    diceNow[diceWayIndex + 1] = otherSideOfDice(diceUp[diceWayIndex]);
   } else if (diceWay[diceWayIndex] == "LEFT") {
     sumOfDiceWay += otherSideOfDice(diceRight[diceWayIndex]);
-    diceRight[diceWayIndex+1] = diceNow[diceWayIndex];
-    diceNow[diceWayIndex+1] = otherSideOfDice(diceRight[diceWayIndex]);
+    diceRight[diceWayIndex + 1] = diceNow[diceWayIndex];
+    diceNow[diceWayIndex + 1] = otherSideOfDice(diceRight[diceWayIndex]);
   } else {
-    console.error("Invalid value in diceWay: "+diceWay[diceWayIndex]);
+    console.error("Invalid value in diceWay: " + diceWay[diceWayIndex]);
   }
 }
 
-function otherSideOfDice(x){
-  return 7-x;
+function otherSideOfDice(x) {
+  return 7 - x;
 }
 
-for (let i = 0; i < diceWay.length; i++) {
-  console.log(diceNow[0]+", "+sumOfDiceWay)
-  simulateDiceWay(i);
+function matheOhneGrenzenTest() {
+  diceWay = ["UP", "UP", "UP", "UP", "UP", "UP"]
+  diceNow = [4];
+  diceUp = [5];
+  diceRight = [1];
+  sumOfDiceWay = diceNow[0];
+
+  outputAll()
+
+  for (let diceWayIndex = 0; diceWayIndex < 64; diceWayIndex++) {
+
+    simulateDiceWay(diceWayIndex)
+    outputAll()
+
+    for (let i = 0; i < 6; i++) {
+      if (diceWay[i] == "UP") {
+        diceWay[i] = "RIGHT";
+      } else {
+        diceWay[i] = "UP";
+        break;
+      }
+    }
+  }
+  for (let i = 0; i < 6; i++) {
+
+  }
 }
-console.log(diceNow[0]+", "+sumOfDiceWay)
+
+function outputAll() {
+  console.log(diceWay)
+  console.log(diceNow)
+  console.log(diceUp)
+  console.log(diceRight)
+  console.log(sumOfDiceWay)
+}
